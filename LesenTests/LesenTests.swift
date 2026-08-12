8f4bb49cbbac437185d9bab878db2acf7d993429
Biorni Hasmegaj <biorni.hasmegaj@st.oth-regensburg.de>
2026-08-12 11:57:18 +0200

    Initial Commit

---
+++ LesenTests/LesenTests.swift
@@ -0,0 +1,19 @@
+//
+//  LesenTests.swift
+//  LesenTests
+//
+//  Created by Biorni on 12.08.26.
+//
+
+import Testing
+@testable import Lesen
+
+struct LesenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
